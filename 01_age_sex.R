076872b06feac2887d8477cc015b911c806f5e15
Johanna Bayer <hannabayer.89@gmail.com>
2023-08-11 13:43:20 +1000

    add overview of age and sex file

---
+++ 01_age_sex.R
@@ -0,0 +1,11 @@
+# association between age and sex and z-scores
+
+load("~/Dropbox/ENIGMA/derivatives/derivatives.RData")
+source("~/Dropbox/ENIGMA/0_Covariates.R")
+
+
+for(column in 2:36){
+  print(colnames(MDD_cov[column]))
+  print(summary(aov(MDD_cov[,column] ~ MDD_cov$Age+ MDD_cov$Sex)))
+}
+

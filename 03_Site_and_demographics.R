4914e86f3fb69a4b4c18be92b35e5b2444b50e2b
Johanna Bayer <hannabayer.89@gmail.com>
2025-01-29 18:26:50 +0100

    changes to write html files

--- 03_Site_and_demographics.R
+++ 03_Site_and_demographics.R
@@ -3,60 +3,68 @@ library(ggpubr)
 library(officer)
 install.packages("nnet")
 library(nnet)
+library(plotly)
+library(htmlwidgets)
 
 
 ## Raw data ENIGMA
 
 ### Load data
 
-
 setwd("~/Dropbox/ENIGMA")
 
-ENIGMA_full <- read.csv("raw_data/Subcort_Cort_lifespan.csv") #read dataset
-covariates<-read.csv("raw_data/Covariates_lifespan.csv")
-covariates<- covariates[!(is.na(covariates$Age)|is.na(covariates$Sex)),]
+ENIGMA_full <-
+  read.csv("raw_data/Subcort_Cort_lifespan.csv") #read dataset
+covariates <- read.csv("raw_data/Covariates_lifespan.csv")
+covariates <-
+  covariates[!(is.na(covariates$Age) | is.na(covariates$Sex)), ]
 
 
 ### Load updates in data
 
-
-setwd("~/Dropbox/ENIGMA")
 #these are the updates
-MODECT_SubCort_Cort<-read.csv("raw_data/MODECT_SubCort_Cort.csv") # replace with existing variables
-CSAN_Basic_Covariates<-read.csv("raw_data/CSAN_Basic_Covariates.csv") 
-CSAN_SubCort_Cort<-read.csv("raw_data/CSAN_SubCort_Cort.csv")
+MODECT_SubCort_Cort <-
+  read.csv("raw_data/MODECT_SubCort_Cort.csv") # replace with existing variables
+CSAN_Basic_Covariates <-
+  read.csv("raw_data/CSAN_Basic_Covariates.csv")
+CSAN_SubCort_Cort <- read.csv("raw_data/CSAN_SubCort_Cort.csv")
 
 ### Merge
 
 #merge covaraites
-covariates<-rbind(covariates[,2:14],CSAN_Basic_Covariates[,c(1:4,6:14)])
+covariates <-
+  rbind(covariates[, 2:14], CSAN_Basic_Covariates[, c(1:4, 6:14)])
 
-ENIGMA_full[grepl( "MODECT" , ENIGMA_full$SubjID  ), ][,c(2:237)]<-MODECT_SubCort_Cort
-ENIGMA_full<-ENIGMA_full[,c(2:238)]
-CSAN_SubCort_Cort$Site<-65
+ENIGMA_full[grepl("MODECT" , ENIGMA_full$SubjID),][, c(2:237)] <-
+  MODECT_SubCort_Cort
+ENIGMA_full <- ENIGMA_full[, c(2:238)]
+CSAN_SubCort_Cort$Site <- 65
 
-colnames(ENIGMA_full)<-colnames(CSAN_SubCort_Cort)
+colnames(ENIGMA_full) <- colnames(CSAN_SubCort_Cort)
 
-ENIGMA_full<-rbind(ENIGMA_full, CSAN_SubCort_Cort)
+ENIGMA_full <- rbind(ENIGMA_full, CSAN_SubCort_Cort)
 
 
 
 ### String split
 
 
-ENIGMA_full$SubjID<-as.character(ENIGMA_full$SubjID)
+ENIGMA_full$SubjID <- as.character(ENIGMA_full$SubjID)
 levels(ENIGMA_full$SubjID)
-ENIGMA_full$Site_name<-str_split(ENIGMA_full$SubjID, "\\_", simplify = TRUE, n=2)[,1]
+ENIGMA_full$Site_name <-
+  str_split(ENIGMA_full$SubjID, "\\_", simplify = TRUE, n = 2)[, 1]
 
-ENIGMA_full$Site_name[grepl( "Episca*" ,ENIGMA_full$SubjID )]<-"Episca"
-ENIGMA_full$Site_name[grepl( "AdF*" ,ENIGMA_full$SubjID )]<-"AdF"
+ENIGMA_full$Site_name[grepl("Episca*" , ENIGMA_full$SubjID)] <-
+  "Episca"
+ENIGMA_full$Site_name[grepl("AdF*" , ENIGMA_full$SubjID)] <- "AdF"
 
-ENIGMA_full$Site_name<-paste0(ENIGMA_full$Site,"_", ENIGMA_full$Site_name)
+ENIGMA_full$Site_name <-
+  paste0(ENIGMA_full$Site, "_", ENIGMA_full$Site_name)
 
-ENIGMA_full$Site_name<-as.factor(ENIGMA_full$Site_name)
+ENIGMA_full$Site_name <- as.factor(ENIGMA_full$Site_name)
 #unique(ENIGMA_full$Site_name)
 
-ENIGMA_full[grepl( "AdF" , ENIGMA_full$SubjID  ), ]
+ENIGMA_full[grepl("AdF" , ENIGMA_full$SubjID),]
 
 
 
@@ -85,7 +93,7 @@ ENIGMA_cov <-
 #sum(ENIGMA_cov$MThickness[ENIGMA_cov$MThickness>10], na.rm=TRUE)
 
 #I cannot use individuals with NA as their disease status
-ENIGMA_cov <- ENIGMA_cov[!is.na(ENIGMA_cov$Dx), ] 
+ENIGMA_cov <- ENIGMA_cov[!is.na(ENIGMA_cov$Dx),]
 
 
 ENIGMA_thick <- ENIGMA_cov[, grep("hick", colnames(ENIGMA_cov))]
@@ -96,8 +104,8 @@ cov_thick <-
 colSums(is.na(ENIGMA_thick))
 
 #exclude all individuals with many values missing
-cov_thick <- cov_thick[!rowSums(is.na(ENIGMA_thick)) > 4, ]
-ENIGMA_thick <- ENIGMA_thick[!rowSums(is.na(ENIGMA_thick)) > 4, ]
+cov_thick <- cov_thick[!rowSums(is.na(ENIGMA_thick)) > 4,]
+ENIGMA_thick <- ENIGMA_thick[!rowSums(is.na(ENIGMA_thick)) > 4,]
 
 
 
@@ -123,14 +131,14 @@ ENIGMA_thick_cov$Site_name <-
 ENIGMA_thick_cov$Site_name <- as.factor(ENIGMA_thick_cov$Site_name)
 levels(ENIGMA_thick_cov$Site_name)
 
-ENIGMA_thick_cov[grepl("AdF" , ENIGMA_thick_cov$SubjID),]
+ENIGMA_thick_cov[grepl("AdF" , ENIGMA_thick_cov$SubjID), ]
 
 
 #unique(ENIGMA_thick_cov$Site_name)
 ## Imputed data ENIGMA, training set
 
 # Display all the sites in the imputed training set:
-  
+
 load("~/Dropbox/ENIGMA/derivatives/imp_random_06_caret.RData")
 sort(unique(imp_train_cont$Site_name))
 
@@ -156,7 +164,6 @@ gghistogram(
 )
 
 
-
 gghistogram(
   imp_test_cont,
   x = "Age",
@@ -179,7 +186,6 @@ gghistogram(
 
 # Get individuals from ENIGMA that are inlcuded in the sample in order to make the plots
 
-
 imp_train_ENIGMA <-
   merge(x = ENIGMA_thick_cov,
         y = imp_train_cont,
@@ -191,6 +197,10 @@ imp_train_ENIGMA <-
 colnames(imp_train_ENIGMA) <-
   c("Age", "Sex", "Dx", "Site", "Site_name")
 imp_train_ENIGMA$Sex <- as.factor(imp_train_ENIGMA$Sex)
+
+imp_train_ENIGMA <- imp_train_ENIGMA %>%
+  mutate(Sex = factor(Sex, levels = c("1", "2"),
+                      labels = c("M", "F")))
 
 p <-
   gghistogram(
@@ -204,27 +214,37 @@ p <-
     palette = c("#00AFBB", "#E7B800")
   )
 
-p_dml <- rvg::dml(ggobj = p)
+# p_dml <- rvg::dml(ggobj = p)
+#
+# # initialize PowerPoint slide ----
+# officer::read_pptx() %>%
+#   # add slide ----
+# officer::add_slide() %>%
+#   # specify object and location of object ----
+# officer::ph_with(p_dml, ph_location()) %>%
+#   # export slide -----
+# base::print(target = here::here("control_train.pptx"))
 
-# initialize PowerPoint slide ----
-officer::read_pptx() %>%
-  # add slide ----
-officer::add_slide() %>%
-  # specify object and location of object ----
-officer::ph_with(p_dml, ph_location()) %>%
-  # export slide -----
-base::print(target = here::here("control_train.pptx"))
+# save to html - interactive plot
+interactive_plot <- ggplotly(p)
+saveWidget(interactive_plot,
+           "figures/train_demographics.html",
+           selfcontained = TRUE)
 
-
-
+#%-------- test controls
 imp_test_ENIGMA <-
   merge(x = ENIGMA_thick_cov,
         y = imp_test_cont,
         by = "SubjID",
         all.y = TRUE)
 
-imp_test_ENIGMA <- imp_test_ENIGMA[, c("Age.x", "Sex.x", "Dx.x", "Site_name.x")]
+imp_test_ENIGMA <-
+  imp_test_ENIGMA[, c("Age.x", "Sex.x", "Dx.x", "Site_name.x")]
 colnames(imp_test_ENIGMA) <- c("Age", "Sex", "Dx", "Site_name")
+imp_test_ENIGMA <- imp_test_ENIGMA %>%
+  mutate(Sex = factor(Sex, levels = c("1", "2"),
+                      labels = c("M", "F")))
+
 imp_test_ENIGMA$Sex <- as.factor(imp_test_ENIGMA$Sex)
 
 p <-
@@ -239,19 +259,25 @@ p <-
     palette = c("#00AFBB", "#E7B800")
   )
 
-p_dml <- rvg::dml(ggobj = p)
+# p_dml <- rvg::dml(ggobj = p)
+#
+# # initialize PowerPoint slide ----
+# officer::read_pptx() %>%
+#   # add slide ----
+# officer::add_slide() %>%
+#   # specify object and location of object ----
+# officer::ph_with(p_dml, ph_location()) %>%
+#   # export slide -----
+# base::print(target = here::here("control_test.pptx"))
 
-# initialize PowerPoint slide ----
-officer::read_pptx() %>%
-  # add slide ----
-officer::add_slide() %>%
-  # specify object and location of object ----
-officer::ph_with(p_dml, ph_location()) %>%
-  # export slide -----
-base::print(target = here::here("control_test.pptx"))
+interactive_plot <- ggplotly(p)
+saveWidget(interactive_plot,
+           "figures/test_demographics.html",
+           selfcontained = TRUE)
 
+#%----
 
-
+# test MDD
 imp_test_MDD_ENIGMA <-
   merge(x = ENIGMA_thick_cov,
         y = imp_test_MDD,
@@ -261,6 +287,10 @@ imp_test_MDD_ENIGMA <-
 imp_test_MDD_ENIGMA <-
   imp_test_MDD_ENIGMA[, c("Age.x", "Sex.x", "Dx.x", "Site_name.x")]
 colnames(imp_test_MDD_ENIGMA) <- c("Age", "Sex", "Dx", "Site_name")
+imp_test_MDD_ENIGMA <- imp_test_MDD_ENIGMA %>%
+  mutate(Sex = factor(Sex, levels = c("1", "2"),
+                      labels = c("M", "F")))
+
 imp_test_MDD_ENIGMA$Sex <- as.factor(imp_test_MDD_ENIGMA$Sex)
 
 p <-
@@ -275,29 +305,38 @@ p <-
     palette = c("#00AFBB", "#E7B800")
   )
 
-p_dml <- rvg::dml(ggobj = p)
+# p_dml <- rvg::dml(ggobj = p)
+#
+# # initialize PowerPoint slide ----
+# officer::read_pptx() %>%
+#   # add slide ----
+# officer::add_slide() %>%
+#   # specify object and location of object ----
+# officer::ph_with(p_dml, ph_location()) %>%
+#   # export slide -----
+# base::print(target = here::here("MDD_test.pptx"))
 
-# initialize PowerPoint slide ----
-officer::read_pptx() %>%
-  # add slide ----
-officer::add_slide() %>%
-  # specify object and location of object ----
-officer::ph_with(p_dml, ph_location()) %>%
-  # export slide -----
-base::print(target = here::here("MDD_test.pptx"))
+interactive_plot <- ggplotly(p)
+saveWidget(
+  interactive_plot,
+  "figures/test_MDD_demographics.html",
+  selfcontained = TRUE,
+  libdir = NULL
+)
 
-## 
+##
 
 ## age sex data set ----------
-## tests 
+## tests
 # sex
 colnames(imp_test_MDD_ENIGMA)
 imp_test_MDD_ENIGMA$Set <- "test_MDD"
 imp_test_ENIGMA$Set <- "test_cont"
 imp_train_ENIGMA$Set <- "train_cont"
 
-df_demo <- rbind(imp_test_ENIGMA[, c("Sex", "Dx","Age", "Site_name", "Set")], imp_train_ENIGMA[, c("Sex", "Dx", "Age", "Site_name", "Set")],
-                imp_test_MDD_ENIGMA[, c("Sex", "Dx", "Age", "Site_name", "Set")])
+df_demo <-
+  rbind(imp_test_ENIGMA[, c("Sex", "Dx", "Age", "Site_name", "Set")], imp_train_ENIGMA[, c("Sex", "Dx", "Age", "Site_name", "Set")],
+        imp_test_MDD_ENIGMA[, c("Sex", "Dx", "Age", "Site_name", "Set")])
 
 
 df_demo$Set <- as.factor(df_demo$Set)
@@ -311,7 +350,8 @@ summary(multi_model)
 exp(coef(multi_model))
 
 # Extract coefficients and standard errors
-z_values <- summary(multi_model)$coefficients / summary(multi_model)$standard.errors
+z_values <-
+  summary(multi_model)$coefficients / summary(multi_model)$standard.errors
 
 # Compute two-tailed p-values
 p_values <- 2 * (1 - pnorm(abs(z_values)))
@@ -321,7 +361,7 @@ p_values
 
 sex_table <- table(df_demo$Sex, df_demo$Set)
 percent_table <- prop.table(sex_table) * 100
-percent_table<- as.data.frame(percent_table)
+percent_table <- as.data.frame(percent_table)
 
 # Column-wise percentages
 col_percent <- prop.table(sex_table, margin = 2) * 100
@@ -330,7 +370,10 @@ print(round(col_percent, 2))
 # age
 
 # Create age groups
-df_demo$age_group <- cut(df_demo$Age, breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90), right = FALSE)
+df_demo$age_group <-
+  cut(df_demo$Age,
+      breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90),
+      right = FALSE)
 
 # Contingency table
 age_table <- table(df_demo$Set, df_demo$age_group)
@@ -342,4 +385,3 @@ perc_table_age
 # Column-wise percentages
 col_percent <- prop.table(age_table, margin = 2) * 100
 print(round(col_percent, 2))
-
